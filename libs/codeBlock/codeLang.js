efb3e74e26957448340a6463ea06e7a5311241c8
Nefelibata <kunpengl0111@gmail.com>
2023-11-29 20:59:03 +0800

    Site updated: 2023-11-29 20:59:02

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,5 +1,4 @@
 // 代码块语言识别
-
 $(function () {
     var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
   
@@ -7,14 +6,8 @@ $(function () {
     $('pre').each(function () {
       var code_language = $(this).attr('class');
   
-  if (!code_language) {
-    return true;
-  };
-  var lang_name = code_language.replace("line-numbers", "").trim().replace("language-", "").trim();
-  
-  // 首字母大写
-  lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
-  $(this).siblings(".code_lang").text(lang_name);
-  
+      var lang_name = String(code_language).replace("line-numbers", "").trim().replace("language-", "").trim();
+      lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
+      $(this).siblings(".code_lang").text(lang_name);
     });
   });
